22b288653fd2d49f28d8159598b52c9c99cf54c8
tfox121 <thomas.fox@compassdigital.io>
2023-05-31 23:59:15 -0400

    fix: improve display of work items

--- src/lib/components/WorkItem.jsx
+++ src/lib/components/WorkItem.jsx
@@ -1,4 +1,4 @@
-import { Box, Button, Stack, Typography } from '@mui/material';
+import { Box, Button, Grid, Stack, Typography } from '@mui/material';
 import Image from 'next/image';
 
 export default function WorkItem({
@@ -10,33 +10,37 @@ export default function WorkItem({
   srcLink,
 }) {
   return (
-    <Box
-      display="flex"
-      alignItems="center"
+    <Grid
+      container
       my={5}
-      flexDirection={index % 2 === 0 ? 'row' : 'row-reverse'}
+      spacing={2}
+      direction={index % 2 === 0 ? 'row' : 'row-reverse'}
     >
-      <Image
-        src={imgSrc}
-        alt="portfolio work screenshot"
-        width="400"
-        height="200"
-      />
-      <Box display="flex" flexDirection="column" pl={2}>
-        <Typography variant="h5">{title}</Typography>
-        <Typography variant="p">{description}</Typography>
-        <Stack direction="row" spacing={2} mt={2}>
-          {appLink && (
-            <Button variant="outlined" m={1} href={appLink} target="_blank">
-              Live App
+      <Grid item xs={12} md={6}>
+        <Image
+          src={imgSrc}
+          alt="portfolio work screenshot"
+          width="400"
+          height="200"
+        />
+      </Grid>
+      <Grid item xs={12} md={6}>
+        <Box display="flex" flexDirection="column" pl={2}>
+          <Typography variant="h5">{title}</Typography>
+          <Typography variant="p">{description}</Typography>
+          <Stack direction="row" spacing={2} mt={2}>
+            {appLink && (
+              <Button variant="outlined" m={1} href={appLink} target="_blank">
+                Live App
+              </Button>
+            )}
+            {/* TODO: Allow for a popup with FE/BE links */}
+            <Button variant="outlined" m={1} href={srcLink} target="_blank">
+              Learn More
             </Button>
-          )}
-          {/* TODO: Allow for a popup with FE/BE links */}
-          <Button variant="outlined" m={1} href={srcLink} target="_blank">
-            Learn More
-          </Button>
-        </Stack>
-      </Box>
-    </Box>
+          </Stack>
+        </Box>
+      </Grid>
+    </Grid>
   );
 }
