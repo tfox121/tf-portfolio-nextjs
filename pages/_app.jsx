76a55eee86b1da6774bf13a54e6718c509b436a6
tfox121 <thomas.fox@compassdigital.io>
2023-05-31 23:52:15 -0400

    feat: ensure initial styling is set correctly

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,8 +1,17 @@
+import Head from 'next/head';
 import { Container } from '@mui/material';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+import theme from '@/src/theme';
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
+    <ThemeProvider theme={theme}>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <CssBaseline />
       <Container maxWidth="md" sx={{ height: '100%' }}>
         <Component {...pageProps} />
       </Container>
@@ -15,6 +24,6 @@ export default function App({ Component, pageProps }) {
           scroll-behavior: smooth;
         }
       `}</style>
-    </>
+    </ThemeProvider>
   );
 }
